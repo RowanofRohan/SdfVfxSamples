d877fc9cf04ede23407b8ff4476dbef09f3a3b12
RowanofRohan <rowan.mcmanus@gmail.com>
2022-01-20 16:52:38 -0600

    initial commit

---
+++ Assets/Temp/CubeDistort.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.VFX.SDF;
+using UnityEngine.UI;
+using MegaFiers;
+
+public class CubeDistort : MonoBehaviour
+{
+
+    [SerializeField]
+    private VisualEffect vfxGraph;
+    // Start is called before the first frame update
+    MegaCrumple crumpler;
+    [SerializeField]
+    private Slider slider;
+
+    MeshToSDFBaker baker;
+    [SerializeField]
+    Mesh cubeMesh;
+    MeshFilter meshRenderer;
+
+    MegaModifyObject modifier;
+
+    
+    public int maxResolution = 64;
+    public Vector3 center;
+    public Vector3 sizeBox;
+    public int signPassCount = 1;
+    public float threshold = 0.5f;
+
+
+
+    void Start()
+    {
+        meshRenderer = this.transform.GetComponent<MeshFilter>();
+        cubeMesh = meshRenderer.sharedMesh;
+        crumpler = this.transform.GetComponent<MegaCrumple>();
+        modifier = this.transform.GetComponent<MegaModifyObject>();
+
+        baker = new MeshToSDFBaker(sizeBox, center, maxResolution, cubeMesh, signPassCount, threshold);
+        baker.BakeSDF();
+        vfxGraph.SetTexture("SDF", baker.SdfTexture);
+    }
+
+    public void UpdateCrumple()
+    {
+        crumpler.scale = slider.value;
+        
+
+        //Update SDF HERE
+    }
+
+
+    private IEnumerator updateSDF()
+    {
+        yield return null;
+        
+        cubeMesh = meshRenderer.sharedMesh;
+        //cubeMesh = modifier.mesh;
+        
+
+        baker.BakeSDF();
+        vfxGraph.SetTexture("SDF", baker.SdfTexture);
+
+    }
+
+    void OnDestroy()
+    {
+        baker.Dispose();
+    }
+}
